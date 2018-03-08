b7a68021c3d9c71c44c657e38f98cbe4171060f1
yoyo930021 <yoyo930021@gmail.com>
2018-03-08 18:47:06 +0800

    Add disconnect alert

--- src/App.vue
+++ src/App.vue
@@ -67,6 +67,10 @@ export default {
       window.socketio.on('connect', () => {
         console.log('connect')
       })
+      this.socket.on('disconnect', () => {
+        console.log('disconnect')
+        this.makeToast('已離線，請重新整理。')
+      })
       window.socketio.on('score', (score) => {
         console.log('score', score)
         self.$store.commit('setScore', score.score)
@@ -81,76 +85,6 @@ export default {
       this.barText = text
       this.snackbar = true
     }
-    // login () {
-    //   this.socket.emit('login', this.token)
-    // },
-    // score () {
-    //   this.socket.emit('score', this.token)
-    // },
-    // rank () {
-    //   this.socket.emit('rank', this.token)
-    // },
-    // chooseLevel () {
-    //   this.socket.emit('chooseLevel')
-    // },
-    // start () {
-    //   this.socket.emit('start', {token: this.token, level: '青銅級'})
-    // },
-    // getProblem () {
-    //   this.socket.emit('getProblem', {token: this.token, roomName: this.room})
-    // },
-    // answear () {
-    //   this.socket.emit('answear', {token: this.token, roomName: this.room, num: 0})
-    //   this.data += 'client answear' + this.counter + '<br>'
-    // },
-    // clear () {
-    //   this.data = ''
-    // },
-    // initSocket () {
-    //   this.socket.on('connect', () => {
-    //     this.data += 'connect<br>'
-    //   })
-    //   this.socket.on('disconnect', () => {
-    //     this.data += 'disconnect<br>'
-    //   })
-    //   this.socket.on('login', (msg) => {
-    //     this.data += 'login' + msg + '<br>'
-    //   })
-    //   this.socket.on('score', (msg) => {
-    //     this.data += 'score' + msg + '<br>'
-    //   })
-    //   this.socket.on('chooseLevel', (msg) => {
-    //     this.data += 'chooseLevel' + msg.toString() + '<br>'
-    //     console.log('chooseLevel', msg)
-    //   })
-    //   this.socket.on('start', (msg) => {
-    //     this.data += 'start' + msg.toString() + '<br>'
-    //     console.log('start', msg)
-    //     this.room = msg
-    //   })
-    //   this.socket.on('getProblem', (msg) => {
-    //     this.data += 'getProblem' + msg.toString() + '<br>'
-    //     console.log('getProblem', msg)
-    //     this.counter = msg.times
-    //     this.count()
-    //   })
-    //   this.socket.on('computer', (msg) => {
-    //     this.data += 'computer' + msg.toString() + this.counter + '<br>'
-    //     console.log('computer', msg)
-    //   })
-    //   this.socket.on('finish', (msg) => {
-    //     this.data += 'finish' + msg.toString() + '<br>'
-    //     console.log('finish', msg)
-    //   })
-    //   this.socket.on('answear', (msg) => {
-    //     this.data += 'answear' + msg.toString() + '<br>'
-    //     console.log('answear', msg)
-    //   })
-    //   this.socket.on('rank', (msg) => {
-    //     this.data += 'rank' + msg.toString() + '<br>'
-    //     console.log('rank', msg)
-    //   })
-    // }
   }
 }
 </script>
