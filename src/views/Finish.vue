8d241404af3b5359ce7b46e39aead651c74cef09
yoyo930021 <yoyo930021@gmail.com>
2018-03-08 15:51:41 +0800

    Fix score equals

--- src/views/Finish.vue
+++ src/views/Finish.vue
@@ -2,7 +2,7 @@
   <div id="Loading">
     <h2 v-if="finish.win" class="amber--text">You win!</h2>
     <h2 v-else class="grey--text">You lose!</h2>
-    <p>{{ finish.playerScore + ((finish.win)?' &gt; ':' &lt; ') + finish.computerScore }}</p>
+    <p>{{ finish.playerScore +  equals() + finish.computerScore }}</p>
     <div style="width: 80%; margin: 50px auto 0px auto;">
       <v-btn block color="cyan" large to="/start">回主頁</v-btn>
       <v-btn block dark color="teal" large to="/round">再玩一次</v-btn>
@@ -20,6 +20,15 @@ export default {
     },
     finish () {
       return this.$store.state.round.finish
+    }
+  },
+  methods: {
+    equals () {
+      if (this.finish.playerScore === this.finish.computerScore) {
+        return ' = '
+      } else {
+        return ((this.finish.win) ? ' > ' : ' < ')
+      }
     }
   },
   beforeMount () {
